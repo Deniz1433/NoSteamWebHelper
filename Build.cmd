6956ad5950fd9984251a62dd59c42b49a5f51f07
Deniz1433 <52750273+Deniz1433@users.noreply.github.com>
2025-12-23 13:05:23 +0300

    Update build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,7 +1,3 @@
 @echo off
-cd "%~dp0/src"
-
-rd /q /s "bin"
-md "bin"
-
-gcc.exe -Oz -Wl,--gc-sections,--exclude-all-symbols -municode -shared -nostdlib -s "Library.c" -lntdll -lwtsapi32 -lkernel32 -luser32 -ladvapi32 -lshell32 -o "bin\umpdc.dll"
+if not exist "bin" md "bin"
+gcc.exe -Oz -Wl,--gc-sections,--exclude-all-symbols -municode -shared -nostdlib -s "DllMain.c" -lntdll -lwtsapi32 -lkernel32 -luser32 -ladvapi32 -lshell32 -o "bin\umpdc.dll"
